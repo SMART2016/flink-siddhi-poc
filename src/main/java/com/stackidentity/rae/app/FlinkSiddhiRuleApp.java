0c695b4d652837bc6924aa40a7b374b7bee9dfee
mazumderd <dipanjan@stackidentity.com>
2021-06-18 15:25:45 +0530

    [Breaking(Compiling but may not run as expected)] Added stream splitting logic and combining related data and rule substreams.

--- src/main/java/com/stackidentity/rae/app/FlinkSiddhiRuleApp.java
+++ src/main/java/com/stackidentity/rae/app/FlinkSiddhiRuleApp.java
@@ -5,6 +5,7 @@ import com.stackidentity.rae.app.config.JobConfig;
 import com.stackidentity.rae.app.config.JobConfigurator;
 import com.stackidentity.rae.app.connector.Producer;
 import com.stackidentity.rae.app.control.ControlStream;
+import com.stackidentity.rae.app.control.model.RuleControlEvent;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -38,6 +39,18 @@ public class FlinkSiddhiRuleApp {
 
         //Initialize siddhi environment with required extensions
         SiddhiCEP cep = configurator.initSiddhiCEPEnv(env);
+
+        //Get input source data streams (All kafka topics for input data)
+        Map<String ,DataStream<?>> inputDataStreams = configurator.getMainInputSourceStreams(env);
+
+        //Get Control streams from rule data sources (All kafka topics for rule)
+        Map<String ,DataStream<RuleControlEvent>> ruleControlStream = configurator.getMainControlSourceStreams(env);
+
+        //Get combined input data and rule stream based on common key in data stream records and rule stream records.
+        Map<String, List<DataStream<?>>> combinedDataAndRuleStream = configurator.getSplittedDataAndControlStream(inputDataStreams,ruleControlStream);
+
+        //TODO: code to generate output streams from the above combined rule and data stream
+        //code ....
 
         //TODO: Split the input stream and rule stream to resource specific streams based on resource types.
         inputAndRuleStreams.forEach((name, lstStreams) -> {
